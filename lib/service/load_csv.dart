875c8a260fef5d605962ac95be8a871777ef21c7
北　恭治 <60954770+kyoji-kita@users.noreply.github.com>
2022-07-07 16:58:24 +0900

    Update load_csv.dart

--- lib/service/load_csv.dart
+++ lib/service/load_csv.dart
@@ -5,6 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:practice/model/quiz.dart';
 import 'package:practice/service/Quiz.dart';
 
+//getCsvDataでid,titleを引っ張れる関数
 Future<List<Map>> getCsvData(String path) async {
   List<Map> quizList = [];
   String csv = await rootBundle.loadString(path);
@@ -22,7 +23,7 @@ Future<List<Map>> getCsvData(String path) async {
   return quizList;
 }
 
-
+//getCsvListで問題を引っ張れる関数
 Future<List<Map>> getCsvList(String path) async {
   List<Map> quizList = [];
   String csv = await rootBundle.loadString(path);
